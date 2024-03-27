87be3278d2259f71761e567ec54d97b0b968bd1c
Julio Cesar <87783694+Julio-Amorim@users.noreply.github.com>
2024-03-27 19:47:58 -0300

    Update desafio10.js

--- desafio_1_semana/desafio10.js
+++ desafio_1_semana/desafio10.js
@@ -2,7 +2,7 @@ let frase = 'LoReM IpSuM Is sImPlY DuMmY TeXt oF ThE PrInTiNg aNd tYpEsEtTiNg iN
 let quantidade = 0;
 
 for (let i = 0; i < frase.length; i++) {
-    if(frase[i] == frase[i].toUpperCase()) {
+    if(frase[i] == frase[i].toUpperCase() && frase[i] != frase[i].toLowerCase()) {
         quantidade++;
     } 
 }
